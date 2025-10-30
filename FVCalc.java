552046d876c8a2bee65573e522156d582195eb4f
Asaf Silver <asaf.silver@post.runi.ac.il>
2025-10-30 11:19:08 +0200

    completed CountTo5

--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,28 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		if (args.length!=3) // מוודא שנקלטים רק 3 ארגומנטים
+		{
+			System.out.println("error");
+			return;
+		}
+		int CurrentValue = Integer.parseInt(args[0]);
+		Double Grow = Double.parseDouble(args[1]);
+		int years = Integer.parseInt(args[2]);
+		if (CurrentValue < 0 || Grow < 0 || Grow > 100 || years < 0) // בודק שכל הארגומנטים תקינים
+		{	System.out.println("error");
+			return;
+		}
+		Grow = Grow / 100; // אחוז הגדילה
+		double futureVal = CurrentValue*Math.pow(1+Grow, years); // מכפיל את הערך הראשוני באחוז הגדילה בחזקת השנים
+		System.out.println("the new value is " + futureVal);
+
+
+
+
+	
+	
+		
+
 	}
 }
